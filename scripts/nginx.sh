56d099f7c8a88d533710ddb2ef35c03e75553092
sahir-jaman <sahirjaman8@gmail.com>
2024-05-20 13:28:14 +0600

    updated ngnix

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,6 +4,9 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
+echo "Removing the default Nginx site if it exists..."
+sudo rm -f /etc/nginx/sites-enabled/default
+
 sudo cp /home/ubuntu/sahir_blogs/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
