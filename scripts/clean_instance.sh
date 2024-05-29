84b346167eecf9a4b4d999add6d2364313ebcc75
sahir-jaman <sahirjaman8@gmail.com>
2024-05-29 11:52:55 +0600

    pdated clean_instance with command

--- scripts/clean_instance.sh
+++ scripts/clean_instance.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Ensure the script is executable
-chmod +x /home/ubuntu/sahir_blogs/scripts/clean_instance.sh
-
 # Remove all contents of the deployment directory
 sudo rm -rf /home/ubuntu/sahir_blogs/*
 
 # Remove existing .gitignore if it exists
 if [ -f /home/ubuntu/sahir_blogs/.gitignore ]; then
-    rm /home/ubuntu/sahir_blogs/.gitignore
+    sudo rm /home/ubuntu/sahir_blogs/.gitignore
 fi
